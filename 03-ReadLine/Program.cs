94d9306f3f0bb5cfc5354e6524b3608387745717
gz729211705 <729211705@qq.com>
2016-11-01 22:32:34 +0800

    添加项目文件。

---
+++ 03-ReadLine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "梦晓，你喜欢吃啥字？";
+            Console.WriteLine(str); 
+            string b = Console.ReadLine();
+            Console.WriteLine("我喜欢吃你喜欢吃的{0}", b);
+            Console.ReadKey();
+        }
+    }
+}
